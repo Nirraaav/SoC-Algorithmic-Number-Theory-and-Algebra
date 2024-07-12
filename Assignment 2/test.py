4b412cca1fe0793f3828635450799264785cfd7a
Nirav <niravbhattad24@gmail.com>
2024-07-12 23:55:37 +0530

    renaming

--- Assignment 2/test.py
+++ Assignment 2/test.py
@@ -1,14 +1,15 @@
 import soc24mathlib
 import random
 
-for i in range(100):
+for i in range(1000000):
     if i % 2 == 0:
-        p = soc24mathlib.gen_prime(100000000000000000000000)
+        p = soc24mathlib.gen_prime(1000000000)
+        # print(p)
         assert(soc24mathlib.aks_test(p) == True)
     else:
-        p = random.randint(10000000000000000000, 10000000000000000000000)
+        p = random.randint(1000000000, 1000000000000)
         assert(soc24mathlib.aks_test(p) == soc24mathlib.is_prime(p))
-
-    if i % 10 == 0:
+        # print(p)
+    if i % 100 == 0:
         print(i)
     
